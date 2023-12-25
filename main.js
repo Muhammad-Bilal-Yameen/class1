fddd665f5cffb7fe71c576564d7a6423eb283177
muhammad bilal yameen <bilalyameen03151190023@gmail.com>
2023-12-24 23:52:28 -0800

    class1

---
+++ main.js
@@ -0,0 +1,12 @@
+//  "use strict";
+// let h = 12
+// let b ="Muhammad"
+// let m = 22
+// let i ="Laiba"
+// console.log(i, m, b, h)
+var x = 24;
+var h = "hamzah";
+var a = 51;
+var b = "amma";
+console.log("my name is " + h + " my age is " + x);
+// console.log(a+b+h+x)
---
+++ main.ts
@@ -0,0 +1,17 @@
+ "use strict";
+ 
+let h = 12
+let b ="Muhammad"
+let m = 22
+let i ="Laiba"
+
+console.log(i, m, b, h)
+
+
+// let x = 24
+// let h = "hamzah"
+// let a = 51
+// let b = "amma"
+
+// console.log("my name is "+  h  +  " my age is "+x)
+// console.log(a+b+h+x)
